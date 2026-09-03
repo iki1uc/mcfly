45b62c3ddc95e9892baec4e15e3ee79a7c767549
iki1uc <entertrainer@web.de>
2026-09-03 20:37:38 +0200

    Add utility functions for energy calculations

--- use.js
+++ use.js
@@ -2,3 +2,12 @@ Pipeline9.useA81 = function(axis){
     this.data = axis.pipe9;
     return this.data;
 };
+function kuckKuck() {
+  return state.step % 2 === 0 ? "KUCK" : "KUCK-KUCK";
+}
+function nanoAtom() {
+  return state.a81 / 9; // feinste Energieeinheit
+}
+function reinEnergie() {
+  return Math.sqrt(state.a81); // Wurzelenergie
+}
