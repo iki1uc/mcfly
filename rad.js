9c46e1dad0731a82d0491b9fecdbebf21b958ecd
iki1uc <entertrainer@web.de>
2026-09-03 20:36:03 +0200

    Update rad.js

--- rad.js
+++ rad.js
@@ -3,3 +3,10 @@ MassHWTranswarp.set(
     iqq,       // Geschwindigkeit
     TimeHW.delta
 );
+function messen() {
+  const aufstieg = state.a81 > 9 ? "HOCH" : "NIEDRIG";
+  const antrieb = state.tmp === "Pump" ? "AKTIV" : "RUHIG";
+  const energie = state.a81;
+
+  return { aufstieg, antrieb, energie };
+}
